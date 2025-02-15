48d9f00aaf6d74a1ecc548df1a57122d7fdf556f
kumaru <kumaru@gamil>
2025-02-15 10:54:44 +0530

    backed S3 added

--- main.tf
+++ main.tf
@@ -6,6 +6,16 @@ resource "aws_instance" "Ec2" {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "my-unique-s3bucket.uday"  # Replace with your actual S3 bucket name
+    key            = "terraform123.tfstate"   # The file name in S3
+    region         = "us-east-1"           # Replace with your AWS region
+    encrypt        = true                  # Enables state file encryption
+  }
+}
+
+
 
 
 
